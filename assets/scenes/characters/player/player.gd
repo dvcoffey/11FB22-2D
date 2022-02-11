14e986067d4920825aa34b0074b43be2fead9f97
dvcoffey <dvcoffey@gmail.com>
2022-02-11 15:30:11 +0000

    Animate player character.
    Add "run" sprite.
    Implement into player.gd.
    Flip sprite for left direction.
    Else play "idle" animation.

--- assets/scenes/characters/player/player.gd
+++ assets/scenes/characters/player/player.gd
@@ -8,8 +8,14 @@ func get_input():
 	velocity = Vector2()
 	if Input.is_action_pressed("right"):
 		velocity.x += 1
-	if Input.is_action_pressed("left"):
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = false
+	elif Input.is_action_pressed("left"):
 		velocity.x -= 1
+		$AnimatedSprite.play("run")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
