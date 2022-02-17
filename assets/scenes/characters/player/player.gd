b7a20159403db2516a1771c6049133b2a7706bb3
dvcoffey <dvcoffey@gmail.com>
2022-02-17 03:06:53 +0000

    Add state machine
    Add state machine to player script for on floor, in air and attacking states

--- assets/scenes/characters/player/player.gd
+++ assets/scenes/characters/player/player.gd
@@ -1,37 +1,78 @@
 extends KinematicBody2D
 
-const SPEED = 150
+enum States {ATTACK = 1, AIR, FLOOR }
+
+const SPEED = 180
 const GRAVITY = 9.8
 const JUMPFORCE = -300
 
 var coins = 0
 var velocity = Vector2(0,0)
+var state = States.AIR
+
 
 func _physics_process(delta):
-	if Input.is_action_pressed("right"):
-		velocity.x = SPEED
-		$AnimatedSprite.play("run")
-		$AnimatedSprite.flip_h = false
-	elif Input.is_action_pressed("left"):
-		velocity.x = -SPEED
-		$AnimatedSprite.play("run")
-		$AnimatedSprite.flip_h = true
-	else:
-		$AnimatedSprite.play("idle")
-	
-	if not is_on_floor():
-		$AnimatedSprite.play("jump")
-	
-	velocity.y = velocity.y + GRAVITY
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMPFORCE
-	
+	match state:
+		States.ATTACK:
+			$AnimatedSprite.play("attack")
+			if Input.is_action_pressed("right"):
+				velocity.x = SPEED
+				$AnimatedSprite.flip_h = false
+			elif Input.is_action_pressed("left"):
+				velocity.x = -SPEED
+				$AnimatedSprite.flip_h = true
 
+		States.AIR:
+			if is_on_floor():
+				state = States.FLOOR
+				continue
+			$AnimatedSprite.play("jump")
+			if Input.is_action_pressed("right"):
+				velocity.x = SPEED
+				$AnimatedSprite.flip_h = false
+			elif Input.is_action_pressed("left"):
+				velocity.x = -SPEED
+				$AnimatedSprite.flip_h = true
+			else:
+				velocity.x = lerp(velocity.x,0,0.2)
+			move_and_fall()
+			if Input.is_action_pressed("attack"):
+				state = States.ATTACK
+		
+		States.FLOOR:
+			if not is_on_floor():
+				state = States.AIR
+				continue
+			if Input.is_action_pressed("right"):
+				$AnimatedSprite.play("run")
+				$AnimatedSprite.flip_h = false
+				velocity.x = SPEED
+			elif Input.is_action_pressed("left"):
+				$AnimatedSprite.play("run")
+				$AnimatedSprite.flip_h = true
+				velocity.x = -SPEED
+			else:
+				$AnimatedSprite.play("idle")
+				velocity.x = lerp(velocity.x,0,0.2)
+			if Input.is_action_just_pressed("jump"):
+				velocity.y = JUMPFORCE
+				state = States.AIR
+			move_and_fall()
+			if Input.is_action_pressed("attack"):
+				state = States.ATTACK
+
+func _on_AnimatedSprite_animation_finished():
+	if not is_on_floor():
+		state = States.AIR
+	else:
+		state = States.FLOOR
+		
+
+
+func move_and_fall():
 	velocity = move_and_slide(velocity,Vector2.UP)
-	
-	velocity.x = lerp(velocity.x,0,0.2)
-	
+	velocity.y = velocity.y + GRAVITY
+		
 
 func _on_fallzone_body_entered(body):
 	get_tree().change_scene("res://11FB22-2D/assets/scenes/levels/level-1.tscn")
@@ -39,7 +80,7 @@ func _on_fallzone_body_entered(body):
 	
 
 func bounce():
-	velocity.y = JUMPFORCE * .5
+	velocity.y = JUMPFORCE
 	
 func hit(var enemyposx):
 	set_modulate(Color(1,0.3,0.3,0.3))
@@ -58,3 +99,8 @@ func hit(var enemyposx):
 
 func _on_Timer_timeout():
 	get_tree().change_scene("res://11FB22-2D/assets/scenes/levels/level-1.tscn")
+
+
+
+
+
